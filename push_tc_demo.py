08d439bae67a70504799b948d5dc73fdf02c3eb1
mashinkareva <mariia.shinkareva@playrix.com>
2024-12-12 23:12:27 +0500

    test pushes from tc

--- push_tc_demo.py
+++ push_tc_demo.py
@@ -1,12 +1,15 @@
 import argparse
 from datetime import datetime, timedelta
 
+import pytz
 from loguru import logger
 
 from one_signal.connect import OneSignalAdapter
 from one_signal.models import FlingPush
 
 if __name__ == "__main__":
+    one_signal = OneSignalAdapter("")
+
     parser = argparse.ArgumentParser(description="Example script with argparse.")
     parser.add_argument("--user_external_id", type=str, help="user_external_id", required=True)
     parser.add_argument("--timezone", type=str, help="timezone", required=True)
@@ -15,14 +18,13 @@ if __name__ == "__main__":
     user_external_id = args.user_external_id
     timezone = args.timezone
 
-    one_signal = OneSignalAdapter("")
-
-    push_time= (datetime.now() + timedelta(minutes=5)).astimezone(timezone).strftime("%H:%M:%S")
+    tz_info =  pytz.timezone(timezone)
+    push_time= (datetime.now() + timedelta(minutes=5)).astimezone(tz_info).strftime("%H:%M:%S")
 
     test_push = [FlingPush(book_id='MarkedJPSina', picture='https://storage.googleapis.com/fling-b827e.appspot.com/Books/MarkedJPSina/90a2fc7c0023de43c4377188ff67c38e377d310e_cover.jpg', text='Dive into 8 new releases, starting with Marked by JP Sina', title='👠 Assert Your Taste in Books! ', user_ids = [f'{user_external_id}'])]
     logger.info(test_push)
 
-    # one_signal.upload_pushes(test_push, push_time)
-    # logger.info('Done')
+    one_signal.upload_pushes(test_push, push_time)
+    logger.info('Done')
 
 
